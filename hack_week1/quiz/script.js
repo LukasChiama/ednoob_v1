6bf06f8cddaf3db74fa3791eec7a858902bcfaa0
Chukwualuka Chiama <lukaschiama@gmail.com>
2019-11-07 16:07:54 +0100

    hack week awka day 4

--- hack_week1/quiz/script.js
+++ hack_week1/quiz/script.js
@@ -1,10 +1,20 @@
 
-function checkAnswer() {
-  const correctAnswer = 'Hypertext Markup Language'
+function checkScore() {
+  let userScore = 0;
+  const correctHTMLAnswer = 'Hypertext Markup Language';
+  const correctCSSAnswer = 'Cascading Style Sheets';
   const htmlAnswer = document.querySelector('input[name=html]:checked').value;
-  if (correctAnswer == htmlAnswer) {
-    alert('Good job. You deserve chewing gum')
+  const cssAnswer = document.querySelector('input[name=css]:checked').value;
+  if (correctHTMLAnswer == htmlAnswer) {
+    userScore += 1;
   } else {
-    alert('Who taught you HTML sef?')
+    document.querySelector("#question-one").style.color = 'red';
   }
+  if (correctCSSAnswer == cssAnswer) {
+   userScore += 1;  
+  } else {
+    const questionTwo = document.getElementById("question-two");
+    questionTwo.style.color = 'red';
+  }
+  alert('Your final score is ' + userScore + '/5');
 }
